525d0a099facbfe8d6c333d9ab9fa6a81aed8928
Yian Li <yianl2@uci.edu>
2019-02-05 16:12:00 -0800

    movement and shooting
    Movement (1%)
    The player can move left & right
    Movement uses the "A", "D", "LeftArrow", and/or "RightArrow" keys
    The player should not be able to move up or down
    The player cannot move past the left/right edges of the screen
    Shooting (1%)
    The player can fire projectiles which travel upwards to kill enemies
    Shooting uses the "Space" key and/or Left Mouse Button
    The player cannot shoot again while a previously fired bullet is still traveling

---
+++ 161 2/Assets/Scripts/space_ship.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class space_ship : MonoBehaviour
+{   
+
+    public float bullet_speed;
+    public float fireRate;
+    public float fireTime;
+    public float count=0;
+    public float speed =200f;
+    public GameObject bullet;
+    public Transform fire_position;
+    private Rigidbody2D rb;
+    private Rigidbody2D bullet_rb;
+    private float axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireTime +=Time.deltaTime;
+        if(Input.GetKeyDown("space")||Input.GetKey(KeyCode.Mouse0)){
+            if(count ==0||(fireTime>fireRate&&count<1)){
+                GameObject b = Instantiate(bullet, fire_position.position,Quaternion.identity);
+                b.GetComponent<Rigidbody2D>().AddForce(Vector2.up*bullet_speed,ForceMode2D.Impulse);
+                count+=1;
+                fireTime =Time.deltaTime;//reset time
+            }
+            
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag =="enemy")
+        {
+            //Destroy(gameObject);
+            //Animator anim =Instantiate(die_animation,other.contacts[0].point, Quaternion.identity).GetComponent<Animator>();
+            //anim.Play(die_animation);
+            //Destroy(anim.gameObject,1.5f);
+        }
+    }
+    void FixedUpdate()
+    {
+        
+        axis = Input.GetAxisRaw("Horizontal");// 0,1,-1
+        rb.velocity = -Vector2.right*axis*speed*Time.deltaTime;
+    }
+}
---
+++ 161 2/Assets/destroy_bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy_bullet : MonoBehaviour
+{   
+    private space_ship s;
+    public GameObject explode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GameObject.FindGameObjectWithTag("player").transform.gameObject.GetComponent<space_ship>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag =="wall")
+        {
+            Vector3 collide_position = other.contacts[0].point;
+            Destroy(gameObject);
+            s.count = 0; //The player cannot shoot again while a previously fired bullet is still traveling
+            GameObject ex =Instantiate(explode,collide_position, Quaternion.identity);
+            ex.transform.Translate(new Vector3(0,0,collide_position.z+0.5f));
+            Animator anim = ex.GetComponent<Animator>();
+            anim.Play("explode");
+            Destroy(anim.gameObject,1.5f);
+        }
+    }
+}
