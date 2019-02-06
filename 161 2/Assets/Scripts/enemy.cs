c0d0b77cd9f6ac8ab5508f1259624cf2cbf6c118
李艺安 <yianl2@uci.edu>
2019-02-05 18:46:04 -0800

    animation for destroy enemy and bullet

---
+++ 161 2/Assets/Scripts/enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public GameObject explode2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag =="bullet"||other.gameObject.tag =="player")
+        {
+           
+            Destroy(gameObject);
+            GameObject ex =Instantiate(explode2,this.transform.position, Quaternion.identity);
+            ex.transform.Translate(new Vector3(0,0,this.transform.position.z+0.5f));
+            Animator anim = ex.GetComponent<Animator>();
+            anim.Play("explode2");
+            Destroy(anim.gameObject,1.5f);
+        }
+    }
+}
