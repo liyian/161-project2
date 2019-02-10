fc6346e43d47eb3c3798d3b3d00c2273cc90538a
Li Zhang <zhangl16@uci.edu>
2019-02-10 15:37:24 -0800

    life

---
+++ 161 2/Assets/lifecounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+                 
+public class lifecounter : MonoBehaviour
+{
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "bullet" || other.gameObject.tag == "player")
+		{
+			health = health - 1;
+            this.GetComponent<Text>().text = health.ToString("3");
+		}
+	}
+}
