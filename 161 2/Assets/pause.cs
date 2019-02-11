85b9785d6a7a3490e0e85fe344140d1b7fafd3a9
Zongde Liu <zongdel@uci.edu>
2019-02-10 21:34:18 -0800

    Pause

---
+++ 161 2/Assets/pause.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class pause : MonoBehaviour
+{
+    private GameObject PausePanel;
+    bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            togglePause();
+        }
+    }
+
+    void togglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            PausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            PausePanel.SetActive(false);
+        }
+    }
+}
